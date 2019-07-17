ff05b21550e50379dd016ae2c437de60bc490c2b
Jimmy.l <jimmy.l@evolutivelabs.com>
2019-07-17 15:46:39 +0800

    ✨ Feat: init the todomvc in typescript

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Todo/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
+import Todo from './components/Todo.vue';
+import Test from './components/Todo.example.vue';
 
 @Component({
   components: {
     HelloWorld,
+    Todo,
+    Test,
   },
 })
 export default class App extends Vue {}
@@ -22,7 +26,6 @@ export default class App extends Vue {}
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
